e8d4c1876f37c3b81ba82057f42aaf01a0ae1d90
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-11-30 21:02:00 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
- 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countOccurrences(const string& str, char target) {
+    int count = 0;
+    for (char c : str) {
+        if (c == target) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    string inputString;
+    char targetChar;
+
+    cout << "Enter a string: ";
+    getline(cin, inputString);
+
+    cout << "Enter the character to count: ";
+    cin >> targetChar;
+
+    int occurrences = countOccurrences(inputString, targetChar);
+
+    cout << "The character '" << targetChar << "' occurs " << occurrences << " times in the string." << endl;
+
+    return 0;
+}
