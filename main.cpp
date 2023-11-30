1dcb24d477ef78dd57128632ab06ee22884c3b20
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-11-30 21:00:38 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
- 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string insertCharacterAt(string str, char character, int position) {
+    if (position >= 0 && position <= str.length()) {
+        str.insert(position, 1, character);
+    }
+    return str;
+}
+
+int main() {
+    string inputString;
+    char charToInsert;
+    int position;
+
+    cout << "Enter a string: ";
+    cin >> inputString;
+
+    cout << "Enter the character to insert: ";
+    cin >> charToInsert;
+
+    cout << "Enter the position to insert the character: ";
+    cin >> position;
+
+    string result = insertCharacterAt(inputString, charToInsert, position);
+
+    cout << "Resulting string: " << result << endl;
+
+    return 0;
+}
