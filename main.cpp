dc61fd776bf20b855de39858dd10173ac691b839
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-11-30 21:10:53 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
- 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char str[100];
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '\t';
+        }
+    }
+
+    cout << str << endl;
+
+    return 0;
+}
