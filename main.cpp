31e178d6699efdcd145eb6f00387bbd72fb64c6f
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-11-30 21:01:24 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
- 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string inputString;
+
+    cout << "Enter a string: ";
+    getline(cin, inputString);
+
+    for (char& c : inputString) {
+        if (c == '.') {
+            c = '!';
+        }
+    }
+
+    cout << "Resulting string: " << inputString << endl;
+
+    return 0;
+}
