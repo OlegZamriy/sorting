d5dc776d01248dd247fb6c0bedda63c945e4b1c0
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-11-30 21:15:07 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
- 
+#include <iostream>
+#include <sstream> 
+using namespace std;
+
+int main() {
+    string inputString;
+
+    cout << "Enter a string: ";
+    getline(cin, inputString);
+
+    stringstream ss(inputString);
+    string word;
+    int wordCount = 0;
+
+    while (ss >> word) {
+        wordCount++;
+    }
+
+    cout << "Number of words: " << wordCount << endl;
+
+    return 0;
+}
