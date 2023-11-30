5d0a30cfd76e929f5241372ea2329a4ed77ba7e6
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-11-30 20:59:47 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
- 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string removeCharacter(string str, char target) {
+    str.erase(remove(str.begin(), str.end(), target), str.end());
+    return str;
+}
+
+int main() {
+    string inputString;
+    char charToRemove;
+
+    cout << "Enter a string: ";
+    cin >> inputString;
+
+    cout << "Enter the character to remove: ";
+    cin >> charToRemove;
+
+    string result = removeCharacter(inputString, charToRemove);
+
+    cout << "Resulting string: " << result << endl;
+
+    return 0;
+}
