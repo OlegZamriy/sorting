bcae671a42bd902de184e8a7ba719f49d42c4b75
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-11-30 20:58:45 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
- 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string removeCharacterAt(string str, int index) {
+    if (index >= 0 && index < str.length()) {
+        str.erase(index, 1);
+    }
+    return str;
+}
+
+int main() {
+    string inputString;
+    int indexToRemove;
+
+    cout << "Enter a string: ";
+    cin >> inputString;
+
+    cout << "Enter the index of the character to remove: ";
+    cin >> indexToRemove;
+
+    string result = removeCharacterAt(inputString, indexToRemove);
+
+    cout << "Resulting string: " << result << endl;
+
+    return 0;
+}
